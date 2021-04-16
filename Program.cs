9085ea3f06d9d5b797d663223ff5ac9b49dc7969
Isaiah-c-alt <73313464+Isaiah-c-alt@users.noreply.github.com>
2021-04-16 12:10:16 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,74 +1,63 @@
-﻿using System;
+using System;
 
-public class Player
-{
-	public string playerName;
-	public string playerSymbol;
-}
+namespace Connect_Four
+{ 
 
-namespace ConnectFour
-{
 	class Program
 	{
-		static int playerDrop(string[,] board, Player currentPlayer, int playerCount)
+		public class Player
+		{
+			public string playerName;
+			public string playerSymbol;
+		}
+
+		public class Computer
+		{
+			public string computerName;
+			public string computerSymbol;
+		}
+
+		static int playerDrop(string[,] board, Player currentPlayer)
 		{
 			int dropChoice;
 
-			if (playerCount == 1)
+			Console.WriteLine(currentPlayer.playerName + "'s Turn ");
+			do
 			{
-				Console.WriteLine(currentPlayer.playerName + "'s Turn ");
-				if (currentPlayer.playerName == "Computer")
-				{
-					var r = new Random();
-					do
-					{
-						Console.WriteLine("Please enter a number between 1 and 7: ");
-						dropChoice = r.Next(1, 8);
-					} while (dropChoice < 1 || dropChoice > 7);
+				Console.WriteLine("Please enter a number between 1 and 7: ");
+				dropChoice = Convert.ToInt32(Console.ReadLine());
+			} while (dropChoice < 1 || dropChoice > 7);
 
-					while (board[1, dropChoice] == "X " || board[1, dropChoice] == "O ")
-					{
-						Console.WriteLine("That row is full, please enter a new row: ");
-						dropChoice = r.Next(1, 8);
-					}
-
-					return dropChoice;
-				}
-				else
-				{
-					do
-					{
-						Console.WriteLine("Please enter a number between 1 and 7: ");
-						dropChoice = Convert.ToInt32(Console.ReadLine());
-					} while (dropChoice < 1 || dropChoice > 7);
-
-					while (board[1, dropChoice] == "X " || board[1, dropChoice] == "O ")
-					{
-						Console.WriteLine("That row is full, please enter a new row: ");
-						dropChoice = Convert.ToInt32(Console.ReadLine());
-					}
-
-					return dropChoice;
-				}
-			}
-			else
+			while (board[1, dropChoice] == "X " || board[1, dropChoice] == "O ")
 			{
-				Console.WriteLine(currentPlayer.playerName + "'s Turn ");
-				do
-				{
-					Console.WriteLine("Please enter a number between 1 and 7: ");
-					dropChoice = Convert.ToInt32(Console.ReadLine());
-				} while (dropChoice < 1 || dropChoice > 7);
-
-				while (board[1, dropChoice] == "X " || board[1, dropChoice] == "O ")
-				{
-					Console.WriteLine("That row is full, please enter a new row: ");
-					dropChoice = Convert.ToInt32(Console.ReadLine());
-				}
-
-				return dropChoice;
-
+				Console.WriteLine("That row is full, please enter a new row: ");
+				dropChoice = Convert.ToInt32(Console.ReadLine());
 			}
+
+			return dropChoice;
+
+		}
+
+		static int computerDrop(string[,] board, Computer currentPlayer)
+        {
+			int dropChoice;
+
+			Console.WriteLine(currentPlayer.computerName + "'s Turn ");
+			var r = new Random();
+			do
+			{
+				Console.WriteLine("Please enter a number between 1 and 7: ");
+				dropChoice = r.Next(1, 8);
+			} while (dropChoice < 1 || dropChoice > 7);
+
+			while (board[1, dropChoice] == "X " || board[1, dropChoice] == "O ")
+			{
+				Console.WriteLine("That row is full, please enter a new row: ");
+				dropChoice = r.Next(1, 8);
+			}
+
+			return dropChoice;
+			
 		}
 
 		static void checkBellow(string[,] board, Player currentPlayer, int dropChoice)
@@ -82,6 +71,29 @@ namespace ConnectFour
 				if (board[length, dropChoice] != "X " && board[length, dropChoice] != "O ")
 				{
 					board[length, dropChoice] = currentPlayer.playerSymbol;
+					turn = 1;
+				}
+				else
+				{
+					--length;
+				}
+
+			} while (turn != 1);
+
+
+		}
+
+		static void computerCheckBellow(string[,] board, Computer currentPlayer, int dropChoice)
+		{
+			int length = 6;
+			int turn = 0;
+
+
+			do
+			{
+				if (board[length, dropChoice] != "X " && board[length, dropChoice] != "O ")
+				{
+					board[length, dropChoice] = currentPlayer.computerSymbol;
 					turn = 1;
 				}
 				else
@@ -119,12 +131,57 @@ namespace ConnectFour
 
 		}
 
-		static int Check(string[,] board, Player currentPlayer)
+		static int check(string[,] board, Player currentPlayer)
 		{
 			string XO;
 			int win;
 
 			XO = currentPlayer.playerSymbol;
+			win = 0;
+
+			for (int i = 8; i >= 1; --i)
+			{
+
+				for (int j = 9; j >= 1; --j)
+				{
+
+					if (board[i, j] == XO && board[i - 1, j - 1] == XO && board[i - 2, j - 2] == XO && board[i - 3, j - 3] == XO)
+					{
+						win = 1;
+					}
+
+					else if (board[i, j] == XO && board[i, j - 1] == XO && board[i, j - 2] == XO && board[i, j - 3] == XO)
+					{
+						win = 1;
+					}
+
+					else if (board[i, j] == XO && board[i - 1, j] == XO && board[i - 2, j] == XO && board[i - 3, j] == XO)
+					{
+						win = 1;
+					}
+
+					else if (board[i, j] == XO && board[i - 1, j + 1] == XO && board[i - 2, j + 2] == XO && board[i - 3, j + 3] == XO)
+					{
+						win = 1;
+					}
+
+					else if (board[i, j] == XO && board[i, j + 1] == XO && board[i, j + 2] == XO && board[i, j + 3] == XO)
+					{
+						win = 1;
+					}
+				}
+
+			}
+
+			return win;
+		}
+
+		static int computerCheck(string[,] board, Computer currentPlayer)
+		{
+			string XO;
+			int win;
+
+			XO = currentPlayer.computerSymbol;
 			win = 0;
 
 			for (int i = 8; i >= 1; --i)
@@ -183,6 +240,11 @@ namespace ConnectFour
 			Console.WriteLine(currentPlayer.playerName + " Connected Four and WON!");
 		}
 
+		static void computerWin(Computer currentPlayer)
+		{
+			Console.WriteLine(currentPlayer.computerName + " Connected Four and WON!");
+		}
+
 		static int restart(string[,] board)
 		{
 			int restart;
@@ -215,15 +277,15 @@ namespace ConnectFour
 			if (playerCount == 1)
 			{
 				Player playerOne = new Player();
-				Player playerTwo = new Player();
+				Computer playerTwo = new Computer();
 				string[,] board = new string[9, 10];
 				int dropChoice, win, full, again;
 
 				Console.WriteLine("Player One please enter your name: ");
 				playerOne.playerName = Console.ReadLine();
 				playerOne.playerSymbol = "X ";
-				playerTwo.playerName = "Computer";
-				playerTwo.playerSymbol = "O ";
+				playerTwo.computerName = "Computer";
+				playerTwo.computerSymbol = "O ";
 
 				full = 0;
 				win = 0;
@@ -232,10 +294,10 @@ namespace ConnectFour
 
 				do
 				{
-					dropChoice = playerDrop(board, playerOne, playerCount);
+					dropChoice = playerDrop(board, playerOne);
 					checkBellow(board, playerOne, dropChoice);
 					displayBoard(board);
-					win = Check(board, playerOne);
+					win = check(board, playerOne);
 					if (win == 1)
 					{
 						playerWin(playerOne);
@@ -246,13 +308,13 @@ namespace ConnectFour
 						}
 					}
 
-					dropChoice = playerDrop(board, playerTwo, playerCount);
-					checkBellow(board, playerTwo, dropChoice);
+					dropChoice = computerDrop(board, playerTwo);
+					computerCheckBellow(board, playerTwo, dropChoice);
 					displayBoard(board);
-					win = Check(board, playerTwo);
+					win = computerCheck(board, playerTwo);
 					if (win == 1)
 					{
-						playerWin(playerTwo);
+						computerWin(playerTwo);
 						again = restart(board);
 						if (again == 2)
 						{
@@ -292,10 +354,10 @@ namespace ConnectFour
 
 				do
 				{
-					dropChoice = playerDrop(board, playerOne, playerCount);
+					dropChoice = playerDrop(board, playerOne);
 					checkBellow(board, playerOne, dropChoice);
 					displayBoard(board);
-					win = Check(board, playerOne);
+					win = check(board, playerOne);
 					if (win == 1)
 					{
 						playerWin(playerOne);
@@ -306,10 +368,10 @@ namespace ConnectFour
 						}
 					}
 
-					dropChoice = playerDrop(board, playerTwo, playerCount);
+					dropChoice = playerDrop(board, playerTwo);
 					checkBellow(board, playerTwo, dropChoice);
 					displayBoard(board);
-					win = Check(board, playerTwo);
+					win = check(board, playerTwo);
 					if (win == 1)
 					{
 						playerWin(playerTwo);
